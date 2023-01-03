bea851c25a5e2d9312f87e39e89d84d24ffc6341
YehyeokBang <qkddpgur318@gmail.com>
2023-01-03 17:17:00 +0900

    feat: Add pure project

---
+++ src/test/java/com/waffle/waffle/WaffleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waffle.waffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaffleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
