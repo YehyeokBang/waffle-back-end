d942d16afe0bd11a26ad193f83a42f594621cb78
YehyeokBang <qkddpgur318@gmail.com>
2023-01-06 15:04:34 +0900

    feat: Add Find All Member data API

--- src/main/java/com/waffle/waffle/controller/AdminController.java
+++ src/main/java/com/waffle/waffle/controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.waffle.waffle.controller;
 
 import com.waffle.waffle.domain.DTO.FineDTO;
+import com.waffle.waffle.domain.DTO.MemberDTO;
+import com.waffle.waffle.domain.Member;
+import com.waffle.waffle.repository.MemberRepository;
 import com.waffle.waffle.service.FineService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,18 +11,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-public class FineController {
+public class AdminController {
+    private final MemberRepository memberRepository;
 
     private final FineService fineService;
 
-    // 관리자 전용 페이지
+    // 관리자 전용 페이지, 모든 멤버의 이름과 파트가 보임
     @GetMapping("/admin")
-    public ResponseEntity<String> admin() {
-        return ResponseEntity.ok("관리자 페이지");
+    public ResponseEntity<List<MemberDTO>> admin() {
+        List<Member> list = memberRepository.findAll();
+        return ResponseEntity.ok(list.stream()
+                .map(Member::toDTO)
+                .collect(Collectors.toList()));
+    }
+
+    // 관리자가 특정 멤버의 내역을 확인
+    @GetMapping("/admin/{memberId}")
+    public ResponseEntity<List<FineDTO>> findByMemberId(@PathVariable("memberId") String memberId) {
+        List<FineDTO> list = fineService.findByMemberId(memberId);
+        return ResponseEntity.ok(list);
     }
 
     // 관리자 페이지에서 벌금 추가하기 RequestBody -> memberId(String), date(YYYY-MM-DD), type(00, 01, 10)
@@ -29,22 +44,8 @@ public class FineController {
         return ResponseEntity.ok("add fine successfully");
     }
 
-    // 테스트용, 관리자 페이지에서 전체 벌금 내역을 문자열로 출력
-    @GetMapping("/admin/list")
-    public ResponseEntity<List<FineDTO>> adminList() {
-        List<FineDTO> list = fineService.findAll();
-        return ResponseEntity.ok(list);
-    }
-
-    // 관리자가 특정 멤버의 내역을 확인
-    @GetMapping("/admin/{memberId}/list")
-    public ResponseEntity<List<FineDTO>> findByMemberId(@PathVariable("memberId") String memberId) {
-        List<FineDTO> list = fineService.findByMemberId(memberId);
-        return ResponseEntity.ok(list);
-    }
-
     // 멤버의 내역 중 선택한 내역의 상태를 변경 (납부/미납)
-    @PatchMapping("/admin/{memberId}/list")
+    @PatchMapping("/admin/{memberId}")
     public ResponseEntity<String> payFine(@PathVariable("memberId") String memberId, @RequestBody FineDTO fineRequestDto) {
         if(fineRequestDto == null) {
             return ResponseEntity.ok("bad request");
@@ -56,7 +57,7 @@ public class FineController {
     }
 
     // 멤버의 내역 중 선택한 내역을 삭제
-    @DeleteMapping("/admin/{memberId}/list")
+    @DeleteMapping("/admin/{memberId}")
     public ResponseEntity<String> deleteFine(@PathVariable("memberId") String memberId, @RequestBody FineDTO fineRequestDto) {
         if(fineRequestDto == null) {
             return ResponseEntity.ok("bad request");
@@ -67,4 +68,10 @@ public class FineController {
         }
     }
 
+    // 테스트용, 관리자 페이지에서 전체 벌금 내역을 문자열로 출력, 테스트용
+    @GetMapping("/admin/list")
+    public ResponseEntity<List<FineDTO>> adminList() {
+        List<FineDTO> list = fineService.findAll();
+        return ResponseEntity.ok(list);
+    }
 }
