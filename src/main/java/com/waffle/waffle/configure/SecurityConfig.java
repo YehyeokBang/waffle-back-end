7e38cd1b7dabe831014049756e7db81941c4d251
YehyeokBang <qkddpgur318@gmail.com>
2023-01-08 13:51:06 +0900

    chore: Edit test file

--- src/main/java/com/waffle/waffle/configure/SecurityConfig.java
+++ src/main/java/com/waffle/waffle/configure/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .and()
 
                 .formLogin()
+                .loginPage("/main")
                 .defaultSuccessUrl("/loginProcess")
 
                 .and()
